461181ccfd4d4fa964f7d4060dce9eb19d075367
Rush-King <114742850+Rush-King@users.noreply.github.com>
2022-10-14 15:06:16 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,18 +7,8 @@ function setup() {
 
 function draw() 
 {
-  background(30);
-  if(keyIsDown(UP_ARROW)){
-    box.position.y=box.position.y+-5;
-  }
-  if(keyIsDown(DOWN_ARROW)){
-    box.position.y=box.position.y+5;
-  }
-  if(keyIsDown(LEFT_ARROW)){
-    box.position.x=box.position.x+-5;
-  }
   if(keyIsDown(RIGHT_ARROW)){
-    box.position.x=box.position.x+5;
+    background ("red")
   }
 drawSprites();
 }

6496263be68a1cb04826ccd689c5fff65d4cf6ff
Rush-King <114742850+Rush-King@users.noreply.github.com>
2022-10-14 15:50:46 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,17 +2,14 @@ var box;
 
 function setup() {
   createCanvas(400,400);
-  box=createSprite(200,200,20,20);
+  box = createSprite (200,200,20,20)
+  background("yellow");
 }
 
 function draw() 
 {
   if(keyIsDown(RIGHT_ARROW)){
-    background ("red")
+    background ("red");
   }
 drawSprites();
 }
-
-
-
-
